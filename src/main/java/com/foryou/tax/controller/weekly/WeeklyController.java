e2573824eddc9495ec1059aa6e9952e9c9a986fd
weiguo.zhang <weiguo.zhang@doosan.com>
2020-04-16 17:03:15 +0800

    upload

--- src/main/java/com/foryou/tax/controller/weekly/WeeklyController.java
+++ src/main/java/com/foryou/tax/controller/weekly/WeeklyController.java
@@ -1,13 +1,32 @@
 package com.foryou.tax.controller.weekly;
 
+import com.foryou.tax.api.excel.ImportExcel;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.tomcat.util.http.fileupload.FileItem;
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.apache.tomcat.util.http.fileupload.RequestContext;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItem;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletRequestContext;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author ：Raymon
@@ -23,7 +42,30 @@ public class WeeklyController {
     }
 
     @RequestMapping(value = "/eleInvoice/importFile", method = {RequestMethod.POST})
-    public void eleInvoiceImport(HttpServletRequest request, HttpServletResponse response, @RequestParam(value="eleUploadFile") CommonsMultipartFile file) {
+    public void eleInvoiceImport(HttpServletRequest request, HttpServletResponse response, @RequestParam(value="eleUploadFile") MultipartFile multipartfile) {
         System.out.println("ele");
+        try {
+            if(!multipartfile.isEmpty()){
+                File file = multipartToFile(multipartfile);
+                System.out.println(file);
+
+                /**
+                 * excel处理
+                 */
+                List<Map<String, Object>> list = ImportExcel.importExcel(file);
+                System.out.println(list);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-}
+
+        public File multipartToFile(MultipartFile multipart) throws IllegalStateException, IOException {
+            File convFile = new File(multipart.getOriginalFilename());
+            convFile.createNewFile();
+            FileOutputStream fos = new FileOutputStream(convFile);
+            fos.write(multipart.getBytes());
+            fos.close();
+            return convFile;
+        }
+    }
