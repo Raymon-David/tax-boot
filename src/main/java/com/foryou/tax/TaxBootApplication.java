1affb3d15e558c9a3fb9754e300a6d6b33ff59ca
weiguo.zhang <weiguo.zhang@doosan.com>
2020-05-09 11:00:34 +0800

    profiles

--- src/main/java/com/foryou/tax/TaxBootApplication.java
+++ src/main/java/com/foryou/tax/TaxBootApplication.java
@@ -1,12 +1,8 @@
 package com.foryou.tax;
 
-import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
 @SpringBootApplication
 public class TaxBootApplication {
 
